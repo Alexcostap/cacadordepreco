40a27efbfecfb4711867c66a6b52d3ef854d379c
Alex Costa <alexcosta_tjs@hotmail.com>
2023-02-17 19:49:27 -0300

    criacao das telas de produtos

---
+++ telaProduto.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tela do Produto</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="icon" href="imagens/icon-logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/footer.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://kit.fontawesome.com/adf9b248e1.css" crossorigin="anonymous">
+    <link rel="shortcut icon" href="imagens/icon-logo.png" type="image/x-icon">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+</head>
+<body> 
+<?php
+    include_once "includes/menu.php"
+?>
+<div class="container">
+<div class="container mt-5 mb-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="images p-3">
+                            <div class="text-center p-4"> 
+                                <img id="main-image" src="imagens/produtos/biscoito_treloso.png" width="250" /> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="product p-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                               <a href="index.php" class="return"> <div class="d-flex align-items-center"> 
+                               <i class="fa fa-long-arrow-left"> </i> <span class="ml-1"> Voltar</span> </div> </a>
+                               <i class="fa fa-shopping-cart text-muted"></i>
+                            </div>
+                            <div class="mt-4 mb-3">
+                                <h5 class="text-uppercase">Biscoito Treloso</h5>
+                                <div class="price d-flex flex-row align-items-center"> <span class="act-price">R$0,75</span>
+                                    
+                                </div>
+                            </div>
+                            <p class="about">Biscoito Treloso do pequeno, varios sabores no precinho lá no Extra bom da Mustardinha</p>
+                            <p class="card-text">
+                                <i class="fa-solid fa-location-dot"></i> <strong>Local:</strong> Mustardinha, Recife - PE <br>
+                                <i class="fa-solid fa-store"></i> <strong>Supermercado:</strong> Extra Bom<br>
+                                <i class="fa-regular fa-money-bill-1"></i> <strong> Valor:</strong> R$ 0,75<br>            
+                            </p>
+                            <div class="cart mt-4 align-items-center"> 
+                                <button class="btn btn-danger text-uppercase mr-2 px-4">Adicionar ao carrinho</button> 
+                            <i class="fa fa-heart text-muted"></i> 
+                            <i class="fa fa-share-alt text-muted"></i> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+
+
+
+
+
+<?php 
+    include_once "includes/rodape.php"
+  ?>   
+</body>
+</html>
